efaa3b63940439bfedeea818f3f5821e8a57462e
shaksworld <shakaaliakwe>
2022-06-20 17:31:13 +0100

    second change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
 var myName = "Shaka Muhammed"
-var height = 222
+var height = 5-6
 var country = "Nigeria"
 
 document.write('My name is' + ' ' + myName + '<br>')
-document.write("I'm "+ ' ' + height + ' ' +'fit tall' +'<br>')
+document.write("I'm "+ ' ' + height + ' ' +'feet tall' +'<br>')
 document.write("I'm from" + ' ' + country)
