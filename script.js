7e0c24184033ae36ae43396b5583b63ac4778b77
shaksworld <shakaaliakwe>
2022-06-20 17:20:12 +0100

    JavaScript Intro

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var myName = "Shaka Muhammed"
+var height = 222
+var country = "Nigeria"
+
+document.write('My name is' + ' ' + myName + '<br>')
+document.write("I'm "+ ' ' + height + ' ' +'fit tall' +'<br>')
+document.write("I'm from" + ' ' + country)
